eaca67c2e13a0b03c0762825e85c5215e056380c
Osmar <49712683+opessanha@users.noreply.github.com>
2026-03-04 22:38:07 -0300

    Update main.tf

--- modules/blog/main.tf
+++ modules/blog/main.tf
@@ -17,7 +17,6 @@ data "aws_ami" "app_ami" {
 
 module "blog_vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  version = "3.14.0"
   name = var.environment.name
   cidr = "${var.environment.network_prefix}.0.0/16"
 
@@ -36,7 +35,7 @@ module "blog_autoscaling_v2" {
   source  = "terraform-aws-modules/autoscaling/aws"
   version = "6.0.0"
 
-  name = "blog-v2"
+  name = "${var.environment.name}-blog"
 
   min_size            = var.asg_min
   max_size            = var.asg_max
@@ -51,7 +50,7 @@ module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
   version = "~> 6.10.0"
 
-  name = "blog-alb"
+  name = "${var.environment.name}-blog-alb"
 
   load_balancer_type = "application"
 
@@ -61,7 +60,7 @@ module "blog_alb" {
 
   target_groups = [
     {
-      name_prefix      = "blog-"
+      name_prefix      = "${var.environment.name}-blog"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
@@ -86,7 +85,7 @@ module "blog_sg" {
   version = "4.13.0"
 
   vpc_id  = module.blog_vpc.vpc_id
-  name    = "blog"
+  name    = "${var.environment.name}-"blog"
   ingress_rules = ["https-443-tcp","http-80-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules = ["all-all"]
